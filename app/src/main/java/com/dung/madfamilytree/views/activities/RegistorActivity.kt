4075f58a986dea319531f3814fab5f74f9830d79
Dung Bui Tuan <96557390+khoailebo@users.noreply.github.com>
2025-03-14 16:12:25 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/dung/madfamilytree/views/activities/RegistorActivity.kt
+++ app/src/main/java/com/dung/madfamilytree/views/activities/RegistorActivity.kt
@@ -7,15 +7,9 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.dung.madfamilytree.R
 
-class RegistorActivity : AppCompatActivity() {
+class RegistorActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_registor)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
