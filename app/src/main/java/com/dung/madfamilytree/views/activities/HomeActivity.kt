a6a486658c72c753b79b759b09781154db94a068
Dung Bui Tuan <96557390+khoailebo@users.noreply.github.com>
2025-03-15 14:06:06 +0700

    Create home activity
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/dung/madfamilytree/views/activities/HomeActivity.kt
+++ app/src/main/java/com/dung/madfamilytree/views/activities/HomeActivity.kt
@@ -5,11 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.dung.madfamilytree.R
+import com.dung.madfamilytree.databinding.ActivityHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : BaseActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpBottonNavBar()
+    }
+    fun setUpBottonNavBar(){
+        binding.bottomNavBarCustom.bottomNavView.setupWithNavController((supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController)
     }
 }
