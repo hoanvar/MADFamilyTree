a01b914bcf2e3363dc0bd8e4a25cc5e8669664e0
Dung Bui Tuan <96557390+khoailebo@users.noreply.github.com>
2025-05-09 11:29:19 +0700

    Revert "Tree manage"

---
+++ app/src/main/java/com/dung/madfamilytree/views/customviews/FamilyTreeNode.kt
@@ -0,0 +1,30 @@
+package com.dung.madfamilytree.views.customviews
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import com.dung.madfamilytree.R
+
+class FamilyTreeNode : LinearLayout {
+    constructor(context: Context) : super(context){
+        setContent(R.layout.family_tree_node,context)
+    }
+    constructor(context: Context,attrs: AttributeSet) : super(context,attrs){
+        setContent(R.layout.family_tree_node,context)
+    }
+    constructor(context: Context,attrs: AttributeSet,defStyleAttr: Int) : super(context,attrs,defStyleAttr)
+    {
+        setContent(R.layout.family_tree_node,context)
+    }
+    constructor(context: Context,attrs: AttributeSet,defStyleAttr: Int,defStyleRes: Int) : super(context,attrs,defStyleAttr,defStyleRes)
+    {
+        setContent(R.layout.family_tree_node,context)
+    }
+    fun setContent(layoutId: Int,context: Context){
+        layoutParams = ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT)
+        LayoutInflater.from(context).inflate(layoutId,this,true)
+    }
+
+}
