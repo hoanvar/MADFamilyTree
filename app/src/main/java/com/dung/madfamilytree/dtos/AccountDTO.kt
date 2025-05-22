c85a6b94a8fca2113d51f1845c6a40bf3bc51c36
Dung Bui Tuan <96557390+khoailebo@users.noreply.github.com>
2025-05-23 00:48:32 +0700

    grand permission

--- app/src/main/java/com/dung/madfamilytree/dtos/AccountDTO.kt
+++ app/src/main/java/com/dung/madfamilytree/dtos/AccountDTO.kt
@@ -4,5 +4,6 @@ data class AccountDTO(
     val id_profile: String = "",
     val password: String = "",
     val username: String = "",
-    val tree_id: String = ""
+    val tree_id: String = "",
+    var id: String = ""
 )
